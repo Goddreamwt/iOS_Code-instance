e87cc28b35e107dc3a47fc9508be822c43c85966
Goddreamwt <zm804890716@163.com>
2017-07-26 15:17:04 +0800

    SQLite学习
    iOS数据库增删改简单Demo

---
+++ iOS开发之SQLite/NameList/NameList/StudentModel.h
@@ -0,0 +1,15 @@
+//
+//  StudentModel.h
+//  NameList
+//
+//  Created by mac on 2017/7/19.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StudentModel : NSObject
+
+@property(nonatomic,strong)NSString * idNum;//学号
+@property(nonatomic,strong)NSString * name; //学生姓名
+@end
